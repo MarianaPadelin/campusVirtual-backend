e0dd8062d44fa7280c0170281040b5992ed85912
Mariana Padelin <marianapadelin@gmail.com>
2025-01-03 17:15:11 -0300

    resolviendo CORS

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:5173",
-    origin: "https://campus-virtual-frontend.vercel.app/",
+    origin: "https://campus-virtual-frontend.vercel.app",
 
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
