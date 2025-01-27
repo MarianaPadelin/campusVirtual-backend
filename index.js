2dd04e5b9532c2d729146625727d834ad3ea5358
Mariana Padelin <marianapadelin@gmail.com>
2025-01-27 09:43:03 -0300

    resolviendo cors

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://campus-virtual-frontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 //Middleware para peticiones por POST y desde body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
